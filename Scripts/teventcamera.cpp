6b4d67fe6c89aaa123cb5328807aa4ec66a87001
D-B-Miller <d.b.miller@sheffield.ac.uk>
2020-10-11 04:18:19 +0100

    + Updated teventcamera.cpp 	- Updated ThermalEventCamera::read 		* No longer accepts variables. The passing of class reference (this) when creating thread provides the necessary links to update the variables. 		* Changed matrix references to use class matrices instead 		* Fixed copy statements copying contents of frame over to last_frame

--- Scripts/teventcamera.cpp
+++ Scripts/teventcamera.cpp
@@ -68,7 +68,7 @@ int ThermalEventCamera::fps()
 void ThermalEventCamera::start(){
 	this->stopThread = false;
 	// create asynchronous thread to read from I2C bus and populate behaviour
-	this->readThread = std::async(std::launch::async,&ThermalEventCamera::threadRead,this->frame,this->last_frame,&this->events);
+	this->readThread = std::async(std::launch::async,&ThermalEventCamera::threadRead,this);
 	this->updateThread = std::async(std::launch::async,&ThermalEventCamera::threadUpdate,this);
 }
 
@@ -78,30 +78,29 @@ void ThermalEventCamera::stop(){
 }
 
 // single read of I2C buff and find element wise
-void ThermalEventCamera::read(static uint16_t* frame, static uint16_t* old_frame,&std::map<int,EventData> eq,){
-	static uint16_t data[834];
+void ThermalEventCamera::read(){
 	uint16_t diff = 0;
 	// get frame data 
-	MLX90640_GetFrameData(MLX_I2C_ADDR,data);
+	MLX90640_GetFrameData(MLX_I2C_ADDR,this->data);
 	// interpolate outliers to create a valid data frame
-	MLX90640_InterpolateOutliers(data, frame);
+	MLX90640_InterpolateOutliers(this->data, this->frame);
 	// check for changes against last frame
 	for(int i=0;i<832;++i)
 	{
-		diff = old_frame[i]-frame[i];
+		diff = this->old_frame[i]-this->frame[i];
 		// if difference between pixels is not zero
 		// add entry to map where index is the pixel index
 		if(diff!=0)
-			eq.insert(std::pair<int,EventData>(i,diff>0? 1 : -1,i));
+			this->events.insert(std::pair<int,EventData>(i,diff>0? 1 : -1,i));
 	}
 	// update last_frame with curent frame
-	std::copy(std::begin(frame),std::end(frame),std::begin(last_frame));
+	std::copy(std::begin(this->frame),std::end(this->frame),std::begin(this->last_frame));
 }
 
 // threaded read of I2C buff
 // repeated calls of read so long as stopThreadis false
 void ThermalEventCamera::threadRead(){
-	while(not stopThread)
+	while(not this->stopThread)
 		this->read();
 }
 
@@ -112,7 +111,7 @@ void ThermalEventCamera::update();
 // threaded updated based on current data
 // runs so long as stopThread is True
 void ThermalEventCamera::threadUpdate(){
-	while(not stopThread)
+	while(not this->stopThread)
 		this->update();
 }
 
