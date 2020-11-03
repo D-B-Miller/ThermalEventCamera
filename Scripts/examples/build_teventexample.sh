8fe1004a66472fbb39805d01525f65627779f0f5
D-B-Miller <d.b.miller@sheffield.ac.uk>
2020-11-03 19:17:43 +0000

    updated build_teventexample.sh. Added object file step

--- Scripts/examples/build_teventexample.sh
+++ Scripts/examples/build_teventexample.sh
@@ -1,2 +1,2 @@
-g++ -std=c++17 -L/home/pi/mlx90640-library-master/libMLX90640_API.a tevent_example.cpp -o tevent_example
-
+g++ -std=c++17 -I ~/mlx90640-library-master/ -c -o ~/Desktop/ThermalEventCamera/Scripts/src/teventcamera.o ~/Desktop/ThermalEventCamera/Scripts/src/teventcamera.cpp
+g++ -std=c++17 -Wall -O2 -I ~/Desktop/ThermalEventCamera/src/ -o /home/pi/Desktop/ThermalEventCamera/Scripts/bin/tevent_example Scripts/src/teventcamera.o Scripts/examples/tevent_example.cpp
