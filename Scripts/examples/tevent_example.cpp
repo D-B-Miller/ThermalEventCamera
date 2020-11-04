6670fb4ac730341e5ddf7adb071821a0e132591d
D-B-Miller <d.b.miller@sheffield.ac.uk>
2020-11-04 17:35:11 +0000

    modified tevent_example.cpp with more comments to explain the different stages

--- Scripts/examples/tevent_example.cpp
+++ Scripts/examples/tevent_example.cpp
@@ -1,5 +1,20 @@
 #include "teventcamera.h"
+#include <iostream>
 
 int main(int argc,char* argv[]){
+	// initialise camera at 32 fps
+	std::cout << "starting camera..." << std::endl;
 	ThermalEventCamera cam(32);
+	std::cout << "starting update and reading thread" << std::endl;
+	cam.start();
+	std::cout << "pausing to allow the threads to do something" << std::endl;
+	for(int i=0;i<5;++i){
+		sleep(1);
+		std::cout << '.' << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << "stopping threads and sleeping for 1s" << std::endl;
+	cam.stop();
+	sleep(1);
+	std::cout << "exiting" << std::endl;
 }
