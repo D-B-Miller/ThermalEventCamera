9c92b69730f6a4c91fdd83561c4b7f1b185851f0
D-B-Miller <d.b.miller@sheffield.ac.uk>
2020-11-13 17:02:44 +0000

    Update teventcamera.cpp - Integrated cmpSet flag into read method. If not set, then the log operation is used to compare the pixels. This is so that the class can be used out of the box. - Compiled tevent_example to test it under threaded and untreaded use case. Works fine

--- Scripts/src/teventcamera.cpp
+++ Scripts/src/teventcamera.cpp
@@ -172,10 +172,23 @@ void ThermalEventCamera::read(){
 	MLX90640_GetFrameData(MLX_I2C_ADDR,this->frame);
 	// check for changes against last frame
 	for(int i=0;i<834;++i)
-	{	// calculate log of frame intensity adding +1 to avoid zero-error
-		if(65535*log(this->frame[i]+1)!=65535*log(this->last_frame[i]+1))
+	{
+		bool cmp = false;
+		// if the compare function is set
+		// use it to compare the target pixel from current and pase frame
+		if(this->cmpSet)
 		{
-			//this->events.insert(std::pair<int,EventData>(i,this->frame[i]>this->last_frame[i]? 1 : -1));
+			cmp = this->compare(this->frame[i],this->last_frame[i]);
+		}
+		else
+		{	// if the compare function has not been set
+			// calculate log of frame intensity adding +1 to avoid zero-error
+			cmp = 65535*log(this->frame[i]+1)!=65535*log(this->last_frame[i]+1);
+		}
+		// if the pixels are different according to the comparison fn
+		// generate EventData and add to queue
+		if(cmp)
+		{
 			this->events.push(EventData(i,this->frame[i]>this->last_frame[i]? 1 : -1));
 		}
 	}
