bdc783c6a3b96de706491c65a7189f7b9d48514f
D-B-Miller <d.b.miller@sheffield.ac.uk>
2020-11-13 16:43:40 +0000

    Updated teventcamera- Added new private variable lts. Most recent timestamp from latest EventData - Added new public method getLatestTS. Retrieves the current value of lts - Added new private variable cmpSet. Flag for indicating if the custom compare function has been set or not. Going to be integrated into the read function. If the flag is false, the log of the current and past pixels is compared. If different then an EventData is created and added to the queue

--- Scripts/src/teventcamera.cpp
+++ Scripts/src/teventcamera.cpp
@@ -150,7 +150,20 @@ void ThermalEventCamera::stop(){
 // and past frames
 void ThermalEventCamera::setCompare(CompareFunc f)
 {
+	// update comparison function
 	this->compare = f;
+	// an uninitialised CompareFunc when compared to 0 returns true
+	// if the function is initialised set flag
+	if(f!=0)
+	{
+		this->cmpSet = true;
+	}
+	else
+	{
+		// if the user has passed a blank comparison function
+		// clear flag
+		this->cmpSet = false;
+	}
 }
 
 // single read of I2C buff and find element wise
@@ -198,6 +211,7 @@ void ThermalEventCamera::update(){
 			// if successful, update sign
 			if(this->events.pop(ev)){
 				this->out[ev.idx] = ev.sign;
+				this->lts = ev.time();
 			}
 		}
 	}
