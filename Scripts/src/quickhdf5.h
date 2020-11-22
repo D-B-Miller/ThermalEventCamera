fc7a71035d59abd5c930a89ccc148f4083f215d1
D-B-Miller <46482002+D-B-Miller@users.noreply.github.com>
2020-11-22 22:39:10 +0000

    Update quickhdf5.h

--- Scripts/src/quickhdf5.h
+++ Scripts/src/quickhdf5.h
@@ -9,19 +9,29 @@ class EventRecorder{
   
         H5::H5File f; // file object
         const auto dtype = H5::PredType::NATIVE_UINT16; // data type of file
+	const auto ttype = H5::PredType::NATIVE_ULONG // data type of time series. set to long for extended recording
   
         void writeFrame(uint16_t frame[834]); // write frame to file
         bool hasTimeSeries(){return this->hasTime;}; // return state of hasTime flag
   
     private:
         bool hasTime = false; // flag to indicate if a time dataset is/has been added
+	/*data dimensions*/
         hsize_t dimsext[2] = {834,1}; // current size of the dataset as we extend it
         hsize_t dims[2] = {834,1}; // starter dimensions
         hsize_t chunk_dims[2] = {834,1}; // dunk dims
         hsize_t offset[2] = {0,0}; // offset for hyperslab
-        H5::DataSpace dataspace(2,dims,maxdims); // dataspace
-	      // dataspace for hyperslab
-	      H5::DataSpace slab;
-	      // enable chunking
-	      H5::DSetCreatPropList cparams;
+	hsize_t maxdims[2] = {834,H5S_UNLIMITED};
+	H5::DataSpace dataspace(2,dims,maxdims); // dataspace for data
+	/*time series dimensions*/
+	hsize_t tdimsext[2] = {834,1}; // current size of the dataset as we extend it
+        hsize_t tdims[2] = {834,1}; // starter dimensions
+        hsize_t tchunk_dims[2] = {834,1}; // dunk dims
+        hsize_t toffset[2] = {0,0}; // offset for hyperslab
+	hsize_t tmaxdims[2] = {834,H5S_UNLIMITED};
+	H5::DataSpace timespace(2,tdims,tmaxdims}
+      	// dataspace for hyperslab
+      	H5::DataSpace slab;
+      	// parameters
+      	H5::DSetCreatPropList dparams,tparams;
 };
