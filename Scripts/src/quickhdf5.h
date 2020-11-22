4f063cc274381b63288d0e3f5fb8365133bde3ff
D-B-Miller <46482002+D-B-Miller@users.noreply.github.com>
2020-11-22 22:39:53 +0000

    Update quickhdf5.h

--- Scripts/src/quickhdf5.h
+++ Scripts/src/quickhdf5.h
@@ -24,11 +24,11 @@ class EventRecorder{
 	hsize_t maxdims[2] = {834,H5S_UNLIMITED};
 	H5::DataSpace dataspace(2,dims,maxdims); // dataspace for data
 	/*time series dimensions*/
-	hsize_t tdimsext[2] = {834,1}; // current size of the dataset as we extend it
-        hsize_t tdims[2] = {834,1}; // starter dimensions
-        hsize_t tchunk_dims[2] = {834,1}; // dunk dims
+	hsize_t tdimsext[2] = {1,1}; // current size of the dataset as we extend it
+        hsize_t tdims[2] = {1,1}; // starter dimensions
+        hsize_t tchunk_dims[2] = {1,1}; // dunk dims
         hsize_t toffset[2] = {0,0}; // offset for hyperslab
-	hsize_t tmaxdims[2] = {834,H5S_UNLIMITED};
+	hsize_t tmaxdims[2] = {1,H5S_UNLIMITED};
 	H5::DataSpace timespace(2,tdims,tmaxdims}
       	// dataspace for hyperslab
       	H5::DataSpace slab;
