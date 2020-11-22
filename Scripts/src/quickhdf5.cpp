a4d297ceab190b3a75c1fa1de360058ce3faee22
D-B-Miller <46482002+D-B-Miller@users.noreply.github.com>
2020-11-22 22:55:13 +0000

    Update quickhdf5.cpp

--- Scripts/src/quickhdf5.cpp
+++ Scripts/src/quickhdf5.cpp
@@ -4,28 +4,28 @@ EventRecorder::EventRecorder(const char* fname,int nfps){
     // open the file
     this->file.openFile(fname,H5F_ACC_TRUNC);
     // update logged fps
-    fps = nfps;
+    this->fps = nfps;
     // update delay between frames
-    this->frame_time_micros = 1000000/fps
+    this->frame_time_micros = 1000000/this->fps;
     /* set dataset parameters */
     // set chunking for time series and data set
-	  dparams.setChunk(2,this->chunk_dims);
+    this->dparams.setChunk(2,this->chunk_dims);
     // set compression
-    dparams.setDeflate(6);
+    this->dparams.setDeflate(6);
     // fill dataset
     uint16_t fill_val = 0;
-    dparams.setFillValue(dtype,&fill_val);
+    this->dparams.setFillValue(this->dtype,&fill_val);
     // create dataset to hold frames
-    dset = this->file.createDataSet("tevent",dtype,dataspace,dparams);
-    dparams.close();
+    this->dset = this->file.createDataSet("tevent",this->dtype,this->dataspacethis->,dparams);
+    this->dparams.close();
     /* set time series dataset */
     // set parameters for dataset
-    tparams.setChunk(2,this->tchunk_dims);
+    this->tparams.setChunk(2,this->tchunk_dims);
     // set compression
-    tparams.setDeflate(6);
+    this->tparams.setDeflate(6);
     unsigned long tfill = 0;
-    tparams.setFillValue(this->ttype,&tfill);
+    this->tparams.setFillValue(this->ttype,&tfill);
     // create dataset to hold time values
-    dset = this->file.createDataSet("tevent",dtype,dataspace,dparams);
-    tparams.close();
+    this->tset = this->file.createDataSet("tevent",this->dtype,this->dataspace,this->dparams);
+    this->tparams.close();
 }
